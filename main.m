976f16c32ae36397f38d744da06b467afc18ab07
Shiva Chidambaram <shivac@mit.edu>
2023-06-13 19:42:10 +0000

    minor polish

--- main.m
+++ main.m
@@ -2,14 +2,10 @@ load "maxgrp_containment_tests.m";
 
 
 function possibly_nonsurj_primes(f : radical_cond := 1, primes_bound := 1000);
-    if radical_cond eq 1 then
-        radical_cond := RadCond(f);
-    end if;
-    badprimes := PrimeFactors(radical_cond);
     f := suppressed_integer_quartic(f);
-    if radical_cond eq 1 then
-        radical_cond := RadCond(f);
-    end if;
+    if radical_cond eq 1 then radical_cond := RadCond(f); end if;
+    badprimes := PrimeFactors(radical_cond);
+    
     possibly_C1image := C1test(f : radical_cond := radical_cond, primes_bound := primes_bound);
     possibly_C3image, possibly_C2image := C2andC3test(f : radical_cond := radical_cond, primes_bound := primes_bound);
     return badprimes, [[2,3,5,7], possibly_C1image, possibly_C3image, possibly_C2image];
