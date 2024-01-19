955c48aa3dc984a64d0a2c8a9f3d793c2670d5d4
Shiva Chidambaram <shivac@mit.edu>
2024-01-19 04:45:27 +0000

    add general Picard curve example

--- final/example.m
+++ final/example.m
@@ -7,7 +7,8 @@ AttachSpec("spec");
 P<x> := PolynomialRing(Integers());
 f := x^4 - x^2 - x + 1;
 possibly_nonsurj_primes(f : radical_cond := 3*23, primes_bound := 1000);
-possibly_nonsurj_primes(f);
+possibly_nonsurj_primes(f); // returns possibly non surjective primes for the Picard curve y^3 = x^4 - x^2 - x + 1
 
 
-
+h := x+1;
+possibly_nonsurj_primes(f,h); // returns possibly non surjective primes for the Picard curve (x + 1) y^3 = x^4 - x^2 - x + 1
