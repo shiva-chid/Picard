442f31ef094591a06a6ab314a1f9c55598683951
Shiva Chidambaram <shivac@mit.edu>
2025-03-04 03:38:13 +0000

    using Frobatinertprimes to find character

--- final/find_onedimchar.m
+++ final/find_onedimchar.m
@@ -1,4 +1,4 @@
-intrinsic find_onedimchar(f :: RngUPolElt, ell :: RngIntElt : radical_cond := 1, primes_bound := 500, ramified := false, uselambdacharpols := false, noskip := true, Hecke_stricteval := true) -> SeqEnum, SeqEnum
+intrinsic find_onedimchar(f :: RngUPolElt, ell :: RngIntElt : radical_cond := 1, primes_bound := 500, charpols := [], ramified := false, useinertFrobsq := false, uselambdacharpols := false, noskip := true, Hecke_stricteval := true) -> SeqEnum, SeqEnum
 {returns list of possible characters of the Galois group of K=Q(zeta_3) that can
 possibly occur in the mod-ell Galois representation of the Jacobian of the curve y^3 = f(x).}
     SetColumns(0);
@@ -34,14 +34,21 @@ possibly occur in the mod-ell Galois representation of the Jacobian of the curve
     isom := hom<omegaF -> F_ellcross | F_ellcross.1>;
     assert #Kernel(isom) eq 1;
 */
-
-    charpols := getcharpols(f : primesend := primes_bound);
-// TODO: can possibly use Bracket-2 of charpols at inert primes.
-    if ramified then
-        charpols := [x : x in charpols | x[1] mod 3 eq 1 and x[1] ne ell];
-    else
-        charpols := [x : x in charpols | x[1] mod (3*ell) eq 1];
+    if charpols eq [] then
+        charpols := getcharpols(f : primesend := primes_bound);
     end if;
+//    print [x[1] : x in charpols];
+    if useinertFrobsq then
+        // using Bracket-2 of charpols at inert primes.
+        charpols := [(x[1] mod 3 eq 1) select x else <x[1],Bracket(2,x[2])> : x in charpols | x[1] ne 3 and x[1] ne ell];
+    else
+        charpols := [x : x in charpols | x[1] mod 3 eq 1 and x[1] ne ell];
+    end if;
+//    print [x[1] : x in charpols];
+    if not ramified then
+        charpols := [x : x in charpols | x[1] mod ell eq 1];
+    end if;
+//    print [x[1] : x in charpols];
     if uselambdacharpols cmpne false then
         resF1_pol := hom<P_F->P_ell|resF1,T>;
         charpolsfacs := [<x[1],Factorisation(ChangeRing(x[2],OF))> : x in charpols];
@@ -59,7 +66,7 @@ possibly occur in the mod-ell Galois representation of the Jacobian of the curve
         charpolsmodell := [<x[1],P_ell ! x[2]> : x in charpols];
     end if;
     primes := [x[1] : x in charpolsmodell];
-//    printf "\nUsing L-polynomials at the (ordinary) primes\n%o\n\n", primes;
+    printf "\nUsing L-polynomials at the (ordinary) primes\n%o\n\n", primes;
 //    print charpolsmodell;
 
     gens_G := SetToSequence(Generators(G));
@@ -79,43 +86,39 @@ it's enough to test only chi or chi^-1.
     number_linearfacs := 6;
     all_roots_charpolsmodell := [];
     while #X gt 0 do
-        p := charpolsmodell[ind,1];
-        charpol := charpolsmodell[ind,2];
-        p_idealfacs := Factorisation(p*OF);
-        p1 := p_idealfacs[1,1];
-        p2 := p_idealfacs[2,1];
-        Append(~prime_ideals,<p1,p2>);
-        if cond mod p ne 0 then
-            roots_charpol := Roots(charpol);
-            if #roots_charpol eq 0 then
-                printf "Witness: p = %o, Frob_p charpol mod %o = %o\n", p, ell, charpol;
-                return [], [];
-            end if;
-            Append(~all_roots_charpolsmodell, <p,&join[{*r[1] : i in [1..r[2]]*} : r in roots_charpol]>);
-            eigvals_rhoell_frobp := [r[1] : r in roots_charpol];
-//            printf "Charpol at p=%o has roots %o over F_%o\n", p, roots_charpol, ell;
-            gens_evalsatp1 := [resmodell((gens_G[i])(p1)) : i in [1..n]];
-            gens_evalsatp2 := [resmodell((gens_G[i])(p2)) : i in [1..n]];
-// Inefficient earlier attempts
-//            Xnew := [x : x in X | incl2(isom((&*[(gens_G[i])^(x[i]) : i in [1..n]])(p1)@@incl1)) in eigvals_rhoell_frobp];
-//            Xnew := [x : x in X | resmodell((&*[(gens_G[i])^(x[i]) : i in [1..n]])(p1)) in eigvals_rhoell_frobp];
-//            Xnew := [x : x in X | resmodell((&*[(gens_G[i])^(x[i]) : i in [1..n]])(p1)) in eigvals_rhoell_frobp or resmodell((&*[(gens_G[i])^(x[i]) : i in [1..n]])(p2)) in eigvals_rhoell_frobp];
-            if Hecke_stricteval then
-                Xnew := [x : x in X | &*[gens_evalsatp1[i]^(x[i]) : i in [1..n]] in eigvals_rhoell_frobp];
-            else
-                Xnew := [x : x in X | &*[gens_evalsatp1[i]^(x[i]) : i in [1..n]] in eigvals_rhoell_frobp or &*[gens_evalsatp2[i]^(x[i]) : i in [1..n]] in eigvals_rhoell_frobp];
-            end if;
-            if noskip or Xnew ne [] then
-                X := Xnew;
-                number_linearfacs := Minimum(number_linearfacs,&+([] cat [r[2] : r in roots_charpol]));
-            end if;
-        end if;
-//        print p, #X;
-        ind := ind+1;
         if ind gt #charpolsmodell then
-            printf "Checked %o primes, up to %o\n", #charpolsmodell, p;
+            printf "Checked %o primes, up to %o\n", #charpolsmodell, charpolsmodell[#charpolsmodell];
             break;
         end if;
+        p := charpolsmodell[ind,1];
+        charpol := charpolsmodell[ind,2];
+        pabove := PrimeIdealsOverPrime(F,p);
+        Append(~prime_ideals,pabove);
+        if cond mod p eq 0 then ind := ind+1; continue; end if;
+        roots_charpol := Roots(charpol);
+        if #roots_charpol eq 0 then
+            printf "Witness: p = %o, Frob_p charpol mod %o = %o has no roots\n", p, ell, charpol;
+            return [], [];
+        end if;
+        Append(~all_roots_charpolsmodell, <p,&join[{*r[1] : i in [1..r[2]]*} : r in roots_charpol]>);
+        eigvals_rhoell_frobp := [r[1] : r in roots_charpol];
+        printf "Charpol at p=%o has roots %o over F_%o\n", p, roots_charpol, ell;
+        gens_evalsatpabove := [[resmodell((gens_G[i])(frakp)) : i in [1..n]] : frakp in pabove];
+// Inefficient earlier attempts
+//        Xnew := [x : x in X | incl2(isom((&*[(gens_G[i])^(x[i]) : i in [1..n]])(pabove[1])@@incl1)) in eigvals_rhoell_frobp];
+//        Xnew := [x : x in X | resmodell((&*[(gens_G[i])^(x[i]) : i in [1..n]])(pabove[1])) in eigvals_rhoell_frobp];
+//        Xnew := [x : x in X | resmodell((&*[(gens_G[i])^(x[i]) : i in [1..n]])(pabove[1])) in eigvals_rhoell_frobp or resmodell((&*[(gens_G[i])^(x[i]) : i in [1..n]])(pabove[2])) in eigvals_rhoell_frobp];
+        if Hecke_stricteval then
+            Xnew := [x : x in X | &*[gens_evalsatpabove[1][i]^(x[i]) : i in [1..n]] in eigvals_rhoell_frobp];
+        else
+            Xnew := [x : x in X | &or[&*[gens_evalsatfrakp[i]^(x[i]) : i in [1..n]] in eigvals_rhoell_frobp : gens_evalsatfrakp in gens_evalsatpabove]];
+        end if;
+        if noskip or Xnew ne [] then
+            X := Xnew;
+            number_linearfacs := Minimum(number_linearfacs,&+([] cat [r[2] : r in roots_charpol]));
+        end if;
+        print p, #X;
+        ind := ind+1;
     end while;
 
     X_chars := <&*[(gens_G[i])^x[i] : i in [1..n]] : x in X>;
@@ -123,12 +126,10 @@ it's enough to test only chi or chi^-1.
     X_chars_values := [];
     for chi in X_chars do
         chi_values_modell := AssociativeArray();
-        for i := 1 to #primes do
-            p := primes[i];
-            p1 := prime_ideals[i][1];
-            p2 := prime_ideals[i][2];
-            chi_values_modell[p] := <resmodell((chi)(p1)),resmodell((chi)(p2))>;
-//            chi_values_modell[p] := incl2(isom((chi)(p1)@@incl1));
+        for i := 1 to #prime_ideals do
+            p := Norm(prime_ideals[i][1] meet Z);
+            chi_values_modell[p] := <resmodell((chi)(frakp)) : frakp in prime_ideals[i]>;
+//            chi_values_modell[p] := incl2(isom((chi)(prime_ideals[i][1])@@incl1));
         end for;
         Append(~X_chars_values,rec<RF | char := chi, values_modell := chi_values_modell>);
     end for;
@@ -174,169 +175,63 @@ it's enough to test only chi or chi^-1.
     return X_chars_values, all_roots_charpolsmodell;
 end intrinsic;
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//     Example: Finding the 1-dim subquotient reps of \ell-torsion representation of a genus 2 Jacobian    //
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/*
-P<x> := PolynomialRing(IntegerRing());
-f := x^4 + x^2 + 1;
-cond := PicardConductor(f);
-n, onedim_subreps_of7tors, all_roots_charpols := find_onedimchar(f,cond,7);
-
-[<Conductor(chi`char), Order(chi`char)> : chi in onedim_subreps_of7tors];
-[Factorisation(Norm(Conductor(AssociatedPrimitiveCharacter(chi`char)))) : chi in onedim_subreps_of7tors];
-
-fields_cutout_over_Qzeta3 := [NumberField(AbelianExtension(chi`char)) : chi in onedim_subreps_of7tors];
-*/
-
-/*
-Why are there 4 characters found for y^3 = x^4-1? It should be 0, 2 or 6, isn't it?
-*/
-
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Example: Finding the 1-dim subquotient reps of \ell-torsion representation of the Jacobian of a Picard curve   //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /*
 AttachSpec("spec");
-P<x> := PolynomialRing(Rationals());
-
-///////////////
-// Example 1 //
-///////////////
-f := P ! [ -142, -387, -147, 338, 243 ]; RadCond(f);
-chars1, allroots := find_onedimchar(f,13);
-chars2, allroots := find_onedimchar(f,13 : noskip := false);
-chars3, allroots := find_onedimchar(f,13 : Hecke_stricteval := false);
-chars4, allroots := find_onedimchar(f,13 : Hecke_stricteval := false, noskip := false);
-chars5, allroots := find_onedimchar(f,13 : uselambdacharpols := true);
-#chars1, #chars2, #chars3, #chars4, #chars5;
-// 0 2 0 200 0
-
-
-///////////////
-// Example 2 //
-///////////////
-
-f := x^4 + 2*x^3 + 4*x^2 + 2*x + 1; RadCond(f);
-chars1, allroots := find_onedimchar(f,7);
-chars2, allroots := find_onedimchar(f,7 : noskip := false);
-chars3, allroots := find_onedimchar(f,7 : Hecke_stricteval := false);
-chars4, allroots := find_onedimchar(f,7 : Hecke_stricteval := false, noskip := false);
-chars5, allroots := find_onedimchar(f,7 : uselambdacharpols := true);
-#chars1, #chars2, #chars3, #chars4, #chars5;
-// 4 4 4 4 2
-
-G := Parent(chars1[1]`char);
-L1 := [G!x`char : x in chars1];
-L2 := [G!x`char : x in chars2];
-L3 := [G!x`char : x in chars3];
-L4 := [G!x`char : x in chars4];
-L5 := [G!x`char : x in chars5];
-L1 eq L2, L1 eq L3, L1 eq L4, Set(L5) subset Set(L1);
-// true true true true
-
-
-ramchars1, allroots := find_onedimchar(f,7 : ramified := true);
-ramchars2, allroots := find_onedimchar(f,7 : ramified := true, noskip := false);
-ramchars3, allroots := find_onedimchar(f,7 : ramified := true, Hecke_stricteval := false);
-ramchars4, allroots := find_onedimchar(f,7 : ramified := true, Hecke_stricteval := false, noskip := false);
-ramchars5, allroots := find_onedimchar(f,7 : ramified := true, uselambdacharpols := true);
-#ramchars1, #ramchars2, #ramchars3, #ramchars4, #ramchars5;
-// 6 6 6 6 3
-
-GG := Parent(ramchars1[1]`char);
-LL1 := [GG!x`char : x in ramchars1];
-LL2 := [GG!x`char : x in ramchars2];
-LL3 := [GG!x`char : x in ramchars3];
-LL4 := [GG!x`char : x in ramchars4];
-LL5 := [GG!x`char : x in ramchars5];
-LL1 eq LL2, LL1 eq LL3, LL1 eq LL4, Set(LL5) subset Set(LL1);
-// true true true true
-
-
-[[Norm(Conductor(ramchi/chi)) : chi in L1] : ramchi in LL1];
+P<x> := PolynomialRing(IntegerRing());
+f := x^4 + x^2 + 1;
+chars, allroots := find_onedimchar(f,7 : ramified := true);
+//
+Orders of the characters generating the Hecke character group:
+[ 6, 6, 6, 6, 6 ]
+Checked 276 primes, up to <4093, t^6 + 6*t^5 + 2*t^4 + 3*t^2 + 3*t + 6>
+Based on the L-polynomials at ORDINARY primes, only the following characters COULD appear as subquotients of rhobar_7:
+Found 2 potential characters. Every Lpoly (factor) used has at least 2 linear factors over F_7.
+//
+G := Parent(chars[1]`char);
+OF := Order(Modulus(G));
+chars := [G!x`char : x in chars]; chars;
 //
 [
-[ 144, 16, 36, 1 ],
-[ 1, 36, 16, 144 ],
-[ 784, 7056, 49, 1764 ],
-[ 2800, 25200, 2800, 25200 ],
-[ 1764, 49, 7056, 784 ],
-[ 25200, 2800, 25200, 2800 ]
+$.1*$.3^3*$.4*$.5^2,
+$.1^4*$.2^5*$.3^2*$.4^4*$.5^4
 ]
 //
 
-// We want to say that the right pairs <unramified,ramified character>
-// correspond to the entries 49=Norm(7) in the above table,
-// since the conductor of the mod-7 cyclotomic character is 7.
-// But it's unclear. See below.
-
-eps := L1[3];
-epschi7 := LL1[3];
-// chi7 := epschi7/eps;
-chi7 := AssociatedPrimitiveCharacter(epschi7/eps);
-
+chi7 := AssociatedPrimitiveCharacter(&*chars);
+assert Conductor(chi7) eq 7*OF;
 K_chi7 := AbsoluteField(NumberField(AbelianExtension(chi7)));
 assert IsNormal(K_chi7) and #Roots(CyclotomicPolynomial(7),K_chi7) eq 6;
-// true
 
-assert eps^-1 eq L1[2]; assert exists(chipow){i : i in [1..6] | LL1[5]/(eps^-1) eq Extend(chi7,GG)^i};
-// false false
+conds := [Conductor(chi) : chi in chars];
+[Norm(cond) : cond in conds];
+// [ 567, 567 ]
+gcdofconds := GCD(conds[1],conds[2]);
+conds := [cond/gcdofconds : cond in conds];
+[Norm(cond) : cond in conds];
+// [ 7, 7 ]
+assert conds[1] ne conds[2];
+assert conds[1]*conds[2] eq 7*OF;
 
-eps2 := L1[2]; assert exists(chipow){i : i in [1..6] | LL1[5]/eps2 eq Extend(chi7,GG)^i}; chipow;
-// 1
-assert AssociatedPrimitiveCharacter(LL1[5]/eps2) eq chi7;
-eps12 := eps*eps2;
-Order(eps12);
-// 2
-Norm(Conductor(eps12));
-// 16
-assert Conductor(eps*eps2) eq 4*RingOfIntegers(CyclotomicField(3));
 
-[Polredabs(DefiningPolynomial(AbsoluteField(NumberField(AbelianExtension(chi))))) : chi in [eps, eps2]];
+orders_chars := [Order(chi) : chi in chars]; orders_chars;
+// [ 6, 6 ]
+fields_chars := [NumberField(AbelianExtension(chi)) : chi in chars]; fields_chars;
 //
 [
-x^12 - 3*x^10 + 9*x^6 + 12*x^4 + 9*x^2 + 3,
-x^12 - 3*x^10 + 9*x^6 + 12*x^4 + 9*x^2 + 3
+Number Field with defining polynomial [ $.1^2 - 4*zeta_3 - 5, $.1^3 - zeta_3 + 4 ] over its ground field,
+Number Field with defining polynomial [ $.1^2 + zeta_3 + 5, $.1^3 - zeta_3 - 5 ] over its ground field
 ]
 //
-[IsNormal(AbsoluteField(NumberField(AbelianExtension(chi)))) : chi in [eps, eps2]];
-// [ false, false ]
-IsIsomorphic(NumberField(AbelianExtension(eps)),NumberField(AbelianExtension(eps2)));
-// true
-K_eps := NumberField(AbelianExtension(eps)); K_eps;
-// Number Field with defining polynomial [ $.1^2 - zeta_3 - 2, $.1^3 + 2 ] over its ground field
-K_eps2 := NumberField(AbelianExtension(eps2)); K_eps2;
-// Number Field with defining polynomial [ $.1^2 + zeta_3 - 1, $.1^3 + 2 ] over its ground field
-defpols_Keps := DefiningPolynomial(K_eps);
-[#Roots(g,K_eps2) : g in defpols_Keps];
-// [ 0, 3 ]
-// So K_eps and K_eps2 are not isomorphic as extensions of F = Q(zeta_3).
-// But there seems to be NO SIMPLE WAY in magma to check for isomorphism of two relative extensions! See below.
-// Calling "NumberField" to create K_eps and K_eps2 seems to be the problem. See below.
-
-
-K_epsrel := RelativeField(BaseField(K_eps),K_eps);
-K_eps2rel := RelativeField(BaseField(K_eps2),K_eps2);
-boo, phi := IsIsomorphic(K_epsrel,K_eps2rel);
-BaseField(K_epsrel).1 eq phi(BaseField(K_epsrel).1);
-// false
-
-F := Domain(DirichletRestriction(G!1));
-K_epsrel := RelativeField(F,K_eps);                  
-K_eps2rel := RelativeField(F,K_eps2);
-boo, phi := IsIsomorphic(K_epsrel,K_eps2rel);
-F.1 eq phi(F.1);
-// false
-
-printf "%m\n%m\n", K_eps, K_eps2;                                
+absolutefields_chars := [AbsoluteField(K) : K in fields_chars]; absolutefields_chars;
 //
-NumberField([ Polynomial(K, [ -K0.1 - 2, 0, 1]), Polynomial(K, [ 2, 0, 0, 1])]:Abs) where K is CyclotomicField(3)
-NumberField([ Polynomial(K, [ K0.1 - 1, 0, 1]), Polynomial(K, [ 2, 0, 0, 1])]:Abs) where K is CyclotomicField(3)
+[
+Number Field with defining polynomial x^12 - 18*x^10 + 18*x^9 + 171*x^8 + 72*x^7 - 849*x^6 - 3780*x^5 + 4914*x^4 + 9954*x^3 + 6615*x^2 + 10584*x + 13671 over the Rational Field,
+Number Field with defining polynomial x^12 + 27*x^10 - 18*x^9 + 306*x^8 - 18*x^7 + 1986*x^6 + 2268*x^5 + 4725*x^4 + 13860*x^3 + 30429*x^2 + 29106*x + 13671 over the Rational Field
+]
 //
-Looks like if I remove the word Abs in the above commands, then magma does infact say the two fields are not isomorphic.
-So I guess when I construct the fields from the Hecke characters I should not call "NumberField", rather something else
-to retain the base field info.
-
-
 */
+
